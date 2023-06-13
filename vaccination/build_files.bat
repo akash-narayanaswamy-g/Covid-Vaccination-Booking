c30455afa9ede9c6ce79d6baf4f94f8296210bcb
Akash Narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-06-14 03:46:12 +0530

    Final commit

--- vaccination/build_files.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Install dependencies
-pip install -r requirements.txt
-
-REM Build static files
-python manage.py collectstatic --noinput
-
-REM Run database migrations
-python manage.py migrate
