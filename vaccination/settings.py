c94cd6482c8ddc3b28f476875cfbbcef517f76b9
Akash Narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-06-14 05:24:42 +0530

    .

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'vaccination.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'HOST':'containers-us-west-34.railway.app',
+        'PORT':'7498',
+        'USER':'root',
+        'PASSWORD':'aFBRwOu5ZxsmsbPrbjUe'
     }
 }
 
