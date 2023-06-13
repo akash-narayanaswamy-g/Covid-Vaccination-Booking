92348a3ffce2b488dd2ebd3bc4a3858a591c2958
Akash Narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-06-14 03:10:26 +0530

    Vercel setup done

---
+++ build_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+#python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
+
+python manage.py runserver
